ba31f2c793be1a72fcfe363aae154b534bea7024
gb1962 <gb1962@wayne.edu>
2019-10-15 20:19:17 -0400

    added create tables function

--- src/csc4710_Espere_part1/InitDatabase.java
+++ src/csc4710_Espere_part1/InitDatabase.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class InitDatabase extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Connection connect = null;
+	private static Connection connect = null;
 	private Statement statement = null;
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
@@ -33,7 +33,7 @@ public InitDatabase() {
 }
 
 //initialize connection to the Server
-protected void connect_function() throws SQLException {
+protected static Connection connect_function() throws SQLException {
 	if(connect == null || connect.isClosed()) {
 		try {
 			Class.forName("come.mysql.jdbc.Driver");
@@ -45,6 +45,58 @@ protected void connect_function() throws SQLException {
 				.getConnection("jdbc:mysql://127.0.0.1:3306/projectdb?"
 	  			          + "user=john&password=pass1234");
 		System.out.println(connect);
+	}
+	return connect;
+}
+
+//create the table
+public void createItemTable() throws SQLException {
+	String dropItemTable = "DROP TABLE IF EXISTS item";
+	String createItemTable = "CREATE TABLE IF NOT EXISTS item" +
+			"(itemID INTEGER, " +
+			"itemDescription VARCHAR(100), " +
+			"itemDate DATE, " +
+			"itemPrice INTEGER, " +
+			"PRIMARY KEY (itemID)";
+	
+	try {
+	connect = InitDatabase.connect_function();
+	statement = connect.createStatement();
+	
+	statement.executeUpdate(dropItemTable);
+	statement.executeUpdate(createItemTable);
+	}
+	
+	catch(SQLException e) {
+	}
+	finally {
+		connect.close();
+	}
+}
+
+public void createUserTable() throws SQLException {
+	String dropUserTable = "DROP TABLE IF EXISTS users";
+	String createUserTable = "CREATE TABLE IF NOT EXISTS users" +
+			"(userID INTEGER, " +
+			"pass VARCHAR(20) NOT NULL, " +
+			"firstName VARCHAR(50), " +
+			"lastName VARCHAR(50), " +
+			"email VARCHAR(50) NOT NULL, " +
+			"age INTEGER, " +
+			"PRIMARY KEY (userID)";
+	
+	try {
+	connect = InitDatabase.connect_function();
+	statement = connect.createStatement();
+	
+	statement.executeUpdate(dropUserTable);
+	statement.executeUpdate(createUserTable);
+
+	}
+	catch (SQLException e) {
+	}
+	finally {
+		connect.close();
 	}
 }
 
